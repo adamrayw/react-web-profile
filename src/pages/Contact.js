c55c3d8b62e612d5498e390a2c6bc6da12635d25
Adam Ray Wibowo <raywibowo68@gmail.com>
2021-12-30 21:03:42 +0700

    using useEffect and add content

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,3 +1,17 @@
+import { useEffect } from "react";
+
 export default function Contact() {
-  return <h1>Contact Page</h1>;
+  useEffect(function () {
+    document.title = "Contact";
+  }, []);
+  return (
+    <section className="section">
+      <h1 className="section-title">Contact Page</h1>
+      <p className="section-description">Klean bisa hubungi que di :</p>
+      <ul>
+        <li>WA: 012345678910</li>
+        <li>FECBOK : Adam Ray Gans</li>
+      </ul>
+    </section>
+  );
 }
