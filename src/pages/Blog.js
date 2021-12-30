8da8179c0f128ca660600ac144d2bcc7cf09abf2
Adam Ray Wibowo <raywibowo68@gmail.com>
2021-12-30 21:01:46 +0700

    import Link from react-router-dom and add title

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function Blog() {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(function () {
+    document.title = "Blog";
+
     async function getArticles() {
       const request = await fetch(
         "https://api.spaceflightnewsapi.net/v3/articles"
@@ -18,14 +21,24 @@ export default function Blog() {
   }, []);
 
   return (
-    <section>
-      <h1>Blog Page</h1>
-      <p>Ini adalah artikel que</p>
-      <hr />
-      {loading ? "Loading..." : ""}
-      {articles.map(function (item) {
-        return <article key={item.id}>{item.title}</article>;
-      })}
+    <section className="section">
+      <h1 className="section-title">Blog Page</h1>
+      <p className="section-description">Ini adalah artikel que, tapi boong.</p>
+      <div className="articles">
+        {loading ? "Loading..." : ""}
+        {articles.map(function (item) {
+          return (
+            <article key={item.id} className="article">
+              <h2 className="article-title">
+                <Link to={`/blog/${item.id}`}>{item.title}</Link>
+              </h2>
+              <time className="article-time">
+                {new Date(item.publishedAt).toLocaleDateString()}
+              </time>
+            </article>
+          );
+        })}
+      </div>
     </section>
   );
 }
