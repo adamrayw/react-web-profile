74a1ad87f70f888827e31d7abb48727ef8b9184c
Adam Ray Wibowo <raywibowo68@gmail.com>
2021-12-30 21:22:48 +0700

    Fix rendered componenet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 // import "./App.css";
 import { Link, Route, Routes } from "react-router-dom";
 import Blog from "./pages/Blog";
+import BlogDetail from "./pages/BlogDetail";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 
 function App() {
   return (
-    <div className="Apps">
-      <nav>
-        <li>
-          <Link to="/">Home</Link>
-          <Link to="/profile">Profile</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/blog">Blog</Link>
-        </li>
+    <div className="app">
+      <div className="brand">Website que</div>
+      <nav className="nav">
+        <Link to="/" className="nav-item">
+          Home
+        </Link>
+        <Link to="/profile" className="nav-item">
+          Profile
+        </Link>
+        <Link to="/contact" className="nav-item">
+          Contact
+        </Link>
+        <Link to="/blog" className="nav-item">
+          Blog
+        </Link>
       </nav>
       <header className="App-header">
         <Routes>
           <Route path="blog" element={<Blog />} />
-          <Route path="blog/:id" element={<Blog />} />
+          <Route path="blog/:id" element={<BlogDetail />} />
           <Route path="contact" element={<Contact />} />
           <Route path="/" element={<Home />} />
           <Route path="profile" element={<Profile />} />
