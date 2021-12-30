dc8c3992167f29b0c1b2e35ae06771baa7ffa45f
Adam Ray Wibowo <raywibowo68@gmail.com>
2021-12-30 21:04:27 +0700

    using useEffect and add content

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,3 +1,15 @@
+import { useEffect } from "react";
+
 export default function Profile() {
-  return <h1>Profile</h1>;
+  useEffect(function () {
+    document.title = "Profile";
+  });
+  return (
+    <section className="section">
+      <h1 className="section-title">Profile Page</h1>
+      <p className="section-description">
+        Hi, nama que adam, que baru aja lulus dari harvard.
+      </p>
+    </section>
+  );
 }
