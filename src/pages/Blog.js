e285307941e8fdae94bb2bf2da384ad63a69b6c5
Adam Ray Wibowo <raywibowo68@gmail.com>
2021-12-30 11:36:29 +0700

    API Integration for get articles

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,3 +1,31 @@
+import { useState, useEffect } from "react";
+
 export default function Blog() {
-  return <h1>Blog Page</h1>;
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(function () {
+    async function getArticles() {
+      const request = await fetch(
+        "https://api.spaceflightnewsapi.net/v3/articles"
+      );
+      const response = await request.json();
+
+      setArticles(response);
+      setLoading(false);
+    }
+    getArticles();
+  }, []);
+
+  return (
+    <section>
+      <h1>Blog Page</h1>
+      <p>Ini adalah artikel que</p>
+      <hr />
+      {loading ? "Loading..." : ""}
+      {articles.map(function (item) {
+        return <article key={item.id}>{item.title}</article>;
+      })}
+    </section>
+  );
 }
